0f73a1938b38ea4c335ca71868896cc06783f3a6
PardusDev <rustemavci@outlook.com.tr>
2024-06-17 02:09:44 +0300

    Some bugs fixed.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,6 +24,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 HoroscopesCard(),
+                SizedBox(height: 30,)
               ],
             ),
           ),
--- lib/widgets/HoroscopeCardButton.dart
+++ lib/widgets/HoroscopeCardButton.dart
@@ -7,45 +7,59 @@ class HoroscopeCardButton extends StatelessWidget {
   const HoroscopeCardButton({super.key, required this.horoscope});
 
   @override
-  Widget build(BuildContext contex) {
-    return ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
-            side: BorderSide(color: Colors.white, width: 1),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25))),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset(horoscope.asset,
-                        )),
-                Text(
-                  horoscope.name.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        double width = constraints.maxWidth * 0.8;
+
+        // TODO: Apply created width variable to sizedbox's width
+        return SizedBox(
+          width: 160,
+          child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  side: BorderSide(color: Colors.white, width: 1),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Container(
+                            width: 70,
+                            height: 70,
+                            child: Image.asset(horoscope.asset,
+                            )),
+                      ),
+                      Text(
+                        horoscope.name.toUpperCase(),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "(" + horoscope.dateRange.toUpperCase() + ")",
+                        style: TextStyle(
+                            color: Colors.white30,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12),
+                      ),
+                      SizedBox(height: 15)
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  "(" + horoscope.dateRange.toUpperCase() + ")",
-                  style: TextStyle(
-                      color: Colors.white30,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 12),
-                ),
-              ],
-            ),
+              )
           ),
-        ));
+        );
+      } );
   }
 }
